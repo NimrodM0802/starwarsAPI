d9251bec3e79c3c7695c0bda69d1066dbb9d846f
NimrodM0802 <nimrodmalnegro@gmail.com>
2019-10-23 12:49:43 +0800

    Updates

---
+++ precache-manifest.538be4b8214774f1fcb823c84aa37232.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e754d6006f4fd776efaabb88f5be755",
+    "url": "/starwarsAPI/index.html"
+  },
+  {
+    "revision": "448c9a17684083492be8",
+    "url": "/starwarsAPI/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c7390968b7ac023417a5",
+    "url": "/starwarsAPI/static/css/main.5d0bc1ab.chunk.css"
+  },
+  {
+    "revision": "448c9a17684083492be8",
+    "url": "/starwarsAPI/static/js/2.e5e2994c.chunk.js"
+  },
+  {
+    "revision": "c7390968b7ac023417a5",
+    "url": "/starwarsAPI/static/js/main.250c3e0b.chunk.js"
+  },
+  {
+    "revision": "96c6640fa5b68ca3f8bb",
+    "url": "/starwarsAPI/static/js/runtime-main.d0fb7272.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/starwarsAPI/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0bcb1ebe5e52ff55fe61e23f23fb78ca",
+    "url": "/starwarsAPI/static/media/staryNight.0bcb1ebe.jpg"
+  }
+]);
